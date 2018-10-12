7ae09ff561b07976a325c28e48d14ef9425ec917
Hamish Cunningham <hamish@gate.ac.uk>
2018-10-12 10:29:17 +0100

    COM3505 weekly update

--- Thing/Thing.ino
+++ Thing/Thing.ino
@@ -7,13 +7,21 @@
 /////////////////////////////////////////////////////////////////////////////
 
 #include "Ex01.h"
+#include "Ex02.h"
+#include "Ex03.h"
+#include "Ex04.h"
+#include "Ex05.h"
 
-int LABNUM = 1; // which lab exercise number are we doing?
+int LABNUM = 5; // which lab exercise number are we doing?
 
 // initialisation entry point
 void setup() {
   switch(LABNUM) {
     case  1: setup01(); break;
+    case  2: setup02(); break;
+    case  3: setup03(); break;
+    case  4: setup04(); break;
+    case  5: setup05(); break;
     default: Serial.println("oops! invalid lab number");
   }
 }
@@ -22,6 +30,10 @@ void setup() {
 void loop() {
   switch(LABNUM) {
     case  1: loop01(); break;
+    case  2: loop02(); break;
+    case  3: loop03(); break;
+    case  4: loop04(); break;
+    case  5: loop05(); break;
     default: Serial.println("oops! invalid lab number");
   }
 }

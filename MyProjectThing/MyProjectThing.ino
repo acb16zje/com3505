c3f1d22ee53a62165891c32e9e0f726fcb86b6e1
njh97 <jhng1@sheffield.ac.uk>
2018-12-02 22:27:17 +0000

    SD server working

--- MyProjectThing/MyProjectThing.ino
+++ MyProjectThing/MyProjectThing.ino
@@ -9,40 +9,67 @@
 #include "Motor.h"
 #include "SDWebServer.h"
 
+void recoverI2C() {   // try to recover I2C bus in case it's locked up...
+  pinMode(SCL, OUTPUT);
+  pinMode(SDA, OUTPUT);
+  digitalWrite(SDA, HIGH);
+
+  for(int i = 0; i < 10; i++) { // 9th cycle acts as NACK
+    digitalWrite(SCL, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(SCL, LOW);
+    delayMicroseconds(5);
+  }
+
+  // a STOP signal (SDA from low to high while SCL is high)
+  digitalWrite(SDA, LOW);
+  delayMicroseconds(5);
+  digitalWrite(SCL, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(SDA, HIGH);
+  delayMicroseconds(2);
+
+  // I2C bus should be free now... a short delay to help things settle
+  delay(200);
+}
+
 // SETUP: initialisation entry point
 void setup() {
-  // Wire.setClock(100000); // higher rates trigger an IOExpander bug
-  // Serial.begin(115200);  // init the serial line
+  recoverI2C();
+  Wire.setClock(100000); // higher rates trigger an IOExpander bug
+  Serial.begin(115200);  // init the serial line
 
-  // // fire up I²C, and the unPhone's IOExpander library
-  // Wire.begin();
-  // IOExpander::begin();
+  // fire up I²C, and the unPhone's IOExpander library
+  Wire.begin();
+  IOExpander::begin();
 
-  // checkPowerSwitch(); // check if power switch is now off & if so shutdown
+  checkPowerSwitch(); // check if power switch is now off & if so shutdown
+  
 
-  // // set up the SD card
-  // IOExpander::digitalWrite(IOExpander::SD_CS, LOW);
-  // if(!SD.begin(-1)) {
-  //   D("Card Mount Failed");
-  //   delay(3000);
-  // }
-  // IOExpander::digitalWrite(IOExpander::SD_CS, HIGH);
-
-  // setupMotor();
-  // startSDWebServer();
+  // set up the SD card
+  IOExpander::digitalWrite(IOExpander::SD_CS, LOW);
+  if(!SD.begin(-1)) {
+    D("Card Mount Failed");
+    delay(3000);
+  }
+  IOExpander::digitalWrite(IOExpander::SD_CS, HIGH);
+  
+  startMotor();
+  startSDWebServer();
 }
 
 // LOOP: task entry point ///////////////////////////////////////////////////
 void loop() {
-//   checkPowerSwitch(); // shutdown if switch off
+  checkPowerSwitch(); // shutdown if switch off
 
-//   // Handle REST calls
-//   client = wifiServer.available();
-//   if (!client) {
-//     return;
-//   }
-//   while(!client.available()){
-//     delay(1);
-//   }
-//   rest.handle(client);
+  // Handle REST calls
+  // client = wifiServer.available();
+  // if (!client) {
+  //   return;
+  // }
+  // while(!client.available()){
+  //   delay(1);
+  // }
+  // rest.handle(client);
+  webServer.handleClient();
 }
--- MyProjectThing/SDWebServer.ino
+++ MyProjectThing/SDWebServer.ino
@@ -5,8 +5,6 @@
 
 #define DBG_OUTPUT_PORT Serial
 
-const char* ssid = "**********";
-const char* password = "**********";
 const char* host = "esp32sd";
 
 static bool hasSD = false;
@@ -21,9 +19,10 @@ void returnFail(String msg) {
 }
 
 bool loadFromSdCard(String path) {
+  IOExpander::digitalWrite(IOExpander::SD_CS, LOW);  
   String dataType = "text/plain";
   if (path.endsWith("/")) {
-    path += "index.htm";
+    path += "demo.htm";
   }
 
   if (path.endsWith(".src")) {
@@ -70,6 +69,7 @@ bool loadFromSdCard(String path) {
   }
 
   dataFile.close();
+  IOExpander::digitalWrite(IOExpander::SD_CS, HIGH);
   return true;
 }
 
@@ -98,46 +98,57 @@ void handleFileUpload() {
 }
 
 void printIndex() {
-   if (!webServer.hasArg("dir")) {
+    IOExpander::digitalWrite(IOExpander::SD_CS, LOW);
+  if (!webServer.hasArg("dir")) {
     return returnFail("BAD ARGS");
   }
   String path = webServer.arg("dir");
-  if (path != "/" && !SD.exists((char *)path.c_str())) {
-    return returnFail("BAD PATH");
-  }
+//   Serial.println(path);
+//   if (path != "/" && !SD.exists((char *)path.c_str())) {
+//     return returnFail("BAD PATH");
+//   }
   File dir = SD.open((char *)path.c_str());
   path = String();
-  if (!dir.isDirectory()) {
-    dir.close();
-    return returnFail("NOT DIR");
-  }
+//   if (!dir.isDirectory()) {
+//     dir.close();
+//     return returnFail("NOT DIR");
+//   }
   dir.rewindDirectory();
   webServer.setContentLength(CONTENT_LENGTH_UNKNOWN);
-  webServer.send(200, "text/json", "");
+  
   WiFiClient client = webServer.client();
-  webServer.sendContent("[");
-  for (int cnt = 0; true; ++cnt) {
-    File entry = dir.openNextFile();
-    if (!entry) {
-      break;
+
+  String content;
+
+   while(dir.available()){
+        content += char(dir.read());
     }
 
-    String output;
-    if (cnt > 0) {
-      output = ',';
-    }
+  webServer.send(200, "text/html", content);
+//   webServer.sendContent("[");
+//   for (int cnt = 0; true; ++cnt) {
+//     File entry = dir.openNextFile();
+//     if (!entry) {
+//       break;
+//     }
 
-    output += "{\"type\":\"";
-    output += (entry.isDirectory()) ? "dir" : "file";
-    output += "\",\"name\":\"";
-    output += entry.name();
-    output += "\"";
-    output += "}";
-    webServer.sendContent(output);
-    entry.close();
-  }
-  webServer.sendContent("]");
+//     String output;
+//     if (cnt > 0) {
+//       output = ',';
+//     }
+
+//     output += "{\"type\":\"";
+//     output += (entry.isDirectory()) ? "dir" : "file";
+//     output += "\",\"name\":\"";
+//     output += entry.name();
+//     output += "\"";
+//     output += "}";
+//     webServer.sendContent(output);
+//     entry.close();
+//   }
+//   webServer.sendContent("]");
   dir.close();
+  IOExpander::digitalWrite(IOExpander::SD_CS, HIGH);
 }
 
 void handleNotFound() {
@@ -159,6 +170,203 @@ void handleNotFound() {
   DBG_OUTPUT_PORT.print(message);
 }
 
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.name(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void createDir(fs::FS &fs, const char * path){
+    Serial.printf("Creating Dir: %s\n", path);
+    if(fs.mkdir(path)){
+        Serial.println("Dir created");
+    } else {
+        Serial.println("mkdir failed");
+    }
+}
+
+void removeDir(fs::FS &fs, const char * path){
+    Serial.printf("Removing Dir: %s\n", path);
+    if(fs.rmdir(path)){
+        Serial.println("Dir removed");
+    } else {
+        Serial.println("rmdir failed");
+    }
+}
+
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if(!file){
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+    file.close();
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+    file.close();
+}
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+    Serial.printf("Renaming file %s to %s\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("File renamed");
+    } else {
+        Serial.println("Rename failed");
+    }
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\n", path);
+    if(fs.remove(path)){
+        Serial.println("File deleted");
+    } else {
+        Serial.println("Delete failed");
+    }
+}
+
+void testFileIO(fs::FS &fs, const char * path){
+    File file = fs.open(path);
+    static uint8_t buf[512];
+    size_t len = 0;
+    uint32_t start = millis();
+    uint32_t end = start;
+    if(file){
+        len = file.size();
+        size_t flen = len;
+        start = millis();
+        while(len){
+            size_t toRead = len;
+            if(toRead > 512){
+                toRead = 512;
+            }
+            file.read(buf, toRead);
+            len -= toRead;
+        }
+        end = millis() - start;
+        Serial.printf("%u bytes read for %u ms\n", flen, end);
+        file.close();
+    } else {
+        Serial.println("Failed to open file for reading");
+    }
+
+
+    file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+
+    size_t i;
+    start = millis();
+    for(i=0; i<2048; i++){
+        file.write(buf, 512);
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+    file.close();
+}
+
+void debugSD() {
+    IOExpander::digitalWrite(IOExpander::SD_CS, LOW);
+    uint8_t cardType = SD.cardType(); 
+
+    if(cardType == CARD_NONE){
+      Serial.println("No SD card attached");
+      return;
+    }
+
+    Serial.print("SD Card Type: ");
+    if(cardType == CARD_MMC){
+        Serial.println("MMC");
+    } else if(cardType == CARD_SD){
+        Serial.println("SDSC");
+    } else if(cardType == CARD_SDHC){
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
+
+    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+    Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
+    listDir(SD, "/", 0);
+    createDir(SD, "/mydir");
+    listDir(SD, "/", 0);
+    removeDir(SD, "/mydir");
+    listDir(SD, "/", 2);
+    writeFile(SD, "/hello.txt", "Hello ");
+    appendFile(SD, "/hello.txt", "World!\n");
+    readFile(SD, "/hello.txt");
+    deleteFile(SD, "/foo.txt");
+    renameFile(SD, "/hello.txt", "/foo.txt");
+    readFile(SD, "/foo.txt");
+    testFileIO(SD, "/test.txt");
+    Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+    Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+    IOExpander::digitalWrite(IOExpander::SD_CS, HIGH);
+}
+
 void startSDWebServer() {
   WiFi.mode(WIFI_AP_STA);
 
@@ -176,5 +384,6 @@ void startSDWebServer() {
   if (SD.begin(SS)) {
     DBG_OUTPUT_PORT.println("SD Card initialized.");
     hasSD = true;
+    debugSD();
   }
 }
